5365b308e48198c7bec13910d394f4a7bb5ccf6b
danagibson <dgibson@san.rr.com>
2021-11-13 13:50:40 -0800

    Update index.js

--- lambda/index.js
+++ lambda/index.js
@@ -4,6 +4,17 @@
  * session persistence, api calls, and more.
  * */
 const Alexa = require('ask-sdk-core');
+let emojiDic = require("emoji-dictionary");
+
+const arrItems = [
+                    "",
+                    "❓","🍫","🌮","🍕","🍟","🥨","🥣","🍦","🍧","🍩","🧁","🍎","🍌","💀","🎃","👻","🎁","🎨","⚽","🏓","🏀","🏐","🏈","🎺","📢","🎬","🧡","👍","🎂","👽","🐹","🐓","🐞","😺","🐶","🐰","🦄","🔎","🎈","🔪",
+                    "🍀","💻","📷","📘","💰","📦","💼","👜","💎","🏆","🙌","💪","🐰","🙈","🙉","🙊"
+        
+                ];
+const arrColors = [
+                    "#FF0000","#FF6A00","#FFD800","#B6FF00","#4CFF00","#00FF21","#00FF90","#00FFFF","#0094FF","#0026FF","#4800FF","#B200FF","#FF00DC","#FF006E","black","white","#202020","#404040","#808080","#A0A0A0"
+                ]
 
 function supportsAPL(handlerInput) {
     const supportedInterfaces = Alexa.getSupportedInterfaces(handlerInput.requestEnvelope);
@@ -20,6 +31,10 @@ const LaunchRequestHandler = {
     },
     handle(handlerInput) {
         const speakOutput = 'Welcome, you can say Bunny Hold, Hello or Help. Which would you like to try?';
+        for (const element of arrItems) 
+        {
+            console.log( emojiDic.getName(element) + "  " + element);
+        }
 
         return handlerInput.responseBuilder
             .addDirective({
@@ -63,6 +78,7 @@ const BunnyHoldIntentHandler = {
         if (supportsAPL(handlerInput)) {
         handlerInput.responseBuilder.addDirective({
             type: 'Alexa.Presentation.APL.RenderDocument',
+            token: 'bunnyToken',
             version: '1.6',
             document: 
             {
@@ -73,90 +89,8 @@ const BunnyHoldIntentHandler = {
             {
                 "aplData":
                 {
-                "inventoryItems": 
-                [
-                    "",
-                    "❓",
-                    "🍫",
-                    "🌮",
-                    "🍕",
-                    "🍟",
-                    "🥨",
-                    "🥣",
-                    "🍦",
-                    "🍧",
-                    "🍩",
-                    "🧁",
-                    "🍎",
-                    "🍌",
-                    "💀",
-                    "🎃",
-                    "👻",
-                    "🎁",
-                    "🎨",
-                    "⚽",
-                    "🏓",
-                    "🏀",
-                    "🏐",
-                    "🏈",
-                    "🎺",
-                    "📢",
-                    "🎬",
-                    "🧡",
-                    "👍",
-                    "🎂",
-                    "👽",
-                    "🐹",
-                    "🐓",
-                    "🐞",
-                    "😺",
-                    "🐶",
-                    "🐰",
-                    "🦄",
-                    "🔎",
-                    "🎈",
-                    "🔪",
-                    "🍀",
-                    "💻",
-                    "📷",
-                    "📘",
-                    "💰",
-                    "📦",
-                    "💼",
-                    "👜",
-                    "💎",
-                    "🏆",
-                    "🙌",
-                    "💪",
-                    "🐰",
-                    "🙈",
-                    "🙉",
-                    "🙊"
-        
-                ],
-                "colors": 
-                [
-                    "#FF0000",
-                    "#FF6A00",
-                    "#FFD800",
-                    "#B6FF00",
-                    "#4CFF00",
-                    "#00FF21",
-                    "#00FF90",
-                    "#00FFFF",
-                    "#0094FF",
-                    "#0026FF",
-                    "#4800FF",
-                    "#B200FF",
-                    "#FF00DC",
-                    "#FF006E",
-                    "black",
-                    "white",
-                    "#202020",
-                    "#404040",
-                    "#808080",
-                    "#A0A0A0"
-                ]
+                "inventoryItems": arrItems,
+                "colors": arrColors
             }
             }
         });
@@ -165,6 +99,182 @@ const BunnyHoldIntentHandler = {
         return handlerInput.responseBuilder
             .speak(speakOutput)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const GrabItemIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'GrabItemIntent';
+    },
+    handle(handlerInput) {
+        const request = Alexa.getRequest(handlerInput.requestEnvelope);
+        console.log("Request is " + JSON.stringify(request));
+        const speakOutput = 'You chose to grab the ' + request.intent.slots.grabbedItem.value;
+        //const testItem = "🏆";
+
+        //console.log("Trophy to text is " + emojiDic.getName(testItem));
+        //console.log("heart eyes emoji from text is " + emojiDic.getUnicode("heart_eyes"));
+        //console.log ("The test item is " + testItem);
+        let itemNameValue = request.intent.slots.grabbedItem.value;
+        console.log("Original item value was: " + request.intent.slots.grabbedItem.value);
+        if (itemNameValue === "jack o. lantern")
+            itemNameValue = "jack_o_lantern";
+        if (itemNameValue.includes(" ")) 
+        {
+            console.log("itemNameValue contains a space");
+            let itemNameValueClean = itemNameValue.replace(/\s/g, '_');
+            itemNameValue = itemNameValueClean;
+        }
+        if (itemNameValue === "positive one")
+            itemNameValue = "+1";
+        console.log("itemNameValue after replace is: " + itemNameValue);
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .addDirective
+            ({
+                type: "Alexa.Presentation.APL.ExecuteCommands",
+                token: "bunnyToken",
+                commands: 
+                [{
+                    type: "SetValue",
+                    componentId: "inventoryItems",
+                    property: "inventoryItem",
+                    value: emojiDic.getUnicode(itemNameValue)
+                }]
+            })
+            .getResponse();
+    }
+};
+
+const SetBackgroundIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'SetBackgroundIntent';
+    },
+    handle(handlerInput) {
+        const request = Alexa.getRequest(handlerInput.requestEnvelope);
+        console.log("Request is " + JSON.stringify(request));
+        let theHashtag = "";
+        console.log("Original colorcode value was: " + request.intent.slots.colorcode.value);
+        if (request.intent.slots.colorcode.value !== "black" && request.intent.slots.colorcode.value !== "white")
+        {
+            theHashtag = "#";
+        }
+        console.log("The hashtag value is " + theHashtag);
+        let backgroundColorCode = theHashtag + request.intent.slots.colorcode.value;
+        let backgroundColorCodeClean = backgroundColorCode.replace(/\s/g, '');
+        backgroundColorCode = backgroundColorCodeClean;
+        const speakOutput = 'You chose to make the background color ' + backgroundColorCode;
+
+        console.log ("The new background color code is " + backgroundColorCode);
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .addDirective
+            ({
+                type: "Alexa.Presentation.APL.ExecuteCommands",
+                token: "bunnyToken",
+                commands: 
+                [{
+                    type: "SetValue",
+                    componentId: "inventoryItems",
+                    property: "backgroundColor",
+                    value: backgroundColorCode
+                }]
+            })
+            .getResponse();
+    }
+};
+
+const SetBunnyColorIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'SetBunnyColorIntent';
+    },
+    handle(handlerInput) {
+        const request = Alexa.getRequest(handlerInput.requestEnvelope);
+        console.log("Request is " + JSON.stringify(request));
+        let theHashtag = "";
+        if (request.intent.slots.bunnycolor.value !== "black" && request.intent.slots.bunnycolor.value !== "white")
+        {
+            theHashtag = "#";
+        }
+        console.log("The hashtag value is " + theHashtag);
+        let bunnyColorCode = theHashtag + request.intent.slots.bunnycolor.value;
+        let bunnyColorCodeClean = bunnyColorCode.replace(/\s/g, '');
+        bunnyColorCode = bunnyColorCodeClean;
+        const speakOutput = 'You chose to make the bunny ' + bunnyColorCode;
+
+        console.log ("The new bunny color code is " + bunnyColorCode);
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .addDirective
+            ({
+                type: "Alexa.Presentation.APL.ExecuteCommands",
+                token: "bunnyyToken",
+                commands: 
+                [{
+                    type: "SetValue",
+                    componentId: "inventoryItems",
+                    property: "bunnyColor",
+                    value: bunnyColorCode
+                }]
+            })
+            .getResponse();
+    }
+};
+
+const DynamicSlotIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'DynamicSlotIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = "Replaced the slots";
+        let replaceEntityDirective = {
+      type: 'Dialog.UpdateDynamicEntities',
+      updateBehavior: 'REPLACE',
+      types: [
+        {
+          name: 'grabableitem',
+          values: [
+            {
+              id: 'slot1',
+              name: {
+                value: 'booze',
+                synonyms: []
+              }
+            },
+            {
+              id: 'slot2',
+              name: {
+                value: 'turkey',
+                synonyms: []
+              }
+            },
+            {
+              id: 'slot3',
+              name: {
+                value: 'chili',
+                synonyms: []
+              }
+            }
+          ]
+        }
+      ]
+    };
+
+//}
+//});
+        
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .addDirective(replaceEntityDirective)
             .getResponse();
     }
 };
@@ -312,6 +422,10 @@ exports.handler = Alexa.SkillBuilders.custom()
         LaunchRequestHandler,
         HelloWorldIntentHandler,
         BunnyHoldIntentHandler,
+        GrabItemIntentHandler,
+        SetBackgroundIntentHandler,
+        SetBunnyColorIntentHandler,
+        DynamicSlotIntentHandler,
         UserEventDecisionHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
